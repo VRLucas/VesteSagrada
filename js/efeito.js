6cc6f6ab8725faad1a47e3d6d1f727d1b27632e7
Lucas Souza <lukkita.lukkita@gmail.com>
2023-06-29 10:38:37 -0300

    html e Css

---
+++ js/efeito.js
@@ -0,0 +1,26 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 0
+        },
+        480: {
+            items: 1
+        },
+        390: {
+            items: 1
+
+        },
+        768: {
+            items: 2
+        },
+        1200: {
+            items: 4
+        }
+
+
+
+    }
+
+});
